02c438f25d4d70a7c1696827b6bb4b9e22231f94
Hao Zhu <prokilchu@gmail.com>
2025-04-21 09:49:47 -0700

    install yazi

--- init.lua
+++ init.lua
@@ -236,7 +236,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  -- NOTE: return Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
@@ -997,6 +997,7 @@ require('lazy').setup({
   require 'custom.plugins.lualine',
   require 'kickstart.plugins.neo-tree',
   require 'custom.plugins.snacks',
+  require 'custom.plugins.yazi',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
